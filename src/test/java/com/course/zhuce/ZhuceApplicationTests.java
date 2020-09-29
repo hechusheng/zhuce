39b308e9804ffdddcd74fc3a6ce56e6475eb7bf5
hechusheng <601826520@qq.com>
2020-09-29 11:51:39 +0800

    Initial commit

---
+++ src/test/java/com/course/zhuce/ZhuceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.zhuce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhuceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
